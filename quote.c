9d99fe9bc856a3e87bebbaaf108a50ac8af2dc70
Aza Bibo <biboaza37@gmail.com>
2023-04-27 18:16:15 +0900

    changed char to unsigned char

--- quote.c
+++ quote.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* extractString(char* str) {
-    char* newStr = malloc(sizeof(char) * strlen(str)); // allocate memory for new string
-    int startIndex = strchr(str, '\"') - str + 1; // find index of first quote and add 1 to skip it
-    int endIndex = strchr(str + startIndex, '\"') - str; // find index of second quote
-    strncpy(newStr, str + startIndex, endIndex - startIndex); // copy substring between quotes to new string
+unsigned char* extractString(unsigned char* str) {
+    unsigned char* newStr = malloc(sizeof(unsigned char) * strlen(str)); // allocate memory for new string
+    int startIndex = strchr((char*)str, '\"') - (char*)str + 1; // find index of first quote and add 1 to skip it
+    int endIndex = strchr((char*)(str + startIndex), '\"') - (char*)str; // find index of second quote
+    strncpy((char*)newStr, (char*)(str + startIndex), endIndex - startIndex); // copy substring between quotes to new string
     newStr[endIndex - startIndex] = '\0'; // null-terminate new string
     return newStr;
 }
 
 int main() {
-    char str[] = "\"5\"";
+    unsigned char str[] = "\"5\"";
     printf("Original string: %s\n", str);
-    char* result = extractString(str);
+    unsigned char* result = extractString(str);
     printf("%s\n", result);
     free(result); // free dynamically allocated memory
     return 0;
